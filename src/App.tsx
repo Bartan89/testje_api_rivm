7c7226e30caa6a7918ae8a24722db696bd17d2c3
Bartan89 <bart.kuijper89@gmail.com>
2020-09-08 19:47:11 +0200

    playing with some covid data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import logo from "./logo.svg"
+import Axios from "axios"
+import "./App.css"
 
 function App() {
+  const [data, fetchData] = useState(false)
+
+  const [fetchedData, setFetchesData] = useState([])
+
+  useEffect((): any => {
+    return async function fetchAPI() {
+      try {
+        console.log("test")
+        const response: any = await Axios.get("https://services.arcgis.com/nSZVuSZjHpEZZbRo/arcgis/rest/services/Coronavirus_RIVM_vlakken_actueel/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=")
+
+        console.log(response.data.features)
+
+        setFetchesData(response.data.features)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+  }, [data])
+
+  function fetchDataFromApi() {
+    fetchData(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <button onClick={fetchDataFromApi}>click me</button>
+
+        <table>
+          {fetchedData.map((element: any) => {
+            return (
+              <>
+                <tr>In {element.attributes.Gemeentenaam}</tr>
+                <tr>Doden: {element.attributes.Overleden}</tr>
+                <tr>Ziekenhuisopnamen: {element.attributes.Ziekenhuisopnamen}</tr>
+              </>
+            )
+          })}
+        </table>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
